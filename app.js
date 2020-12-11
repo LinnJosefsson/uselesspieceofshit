1c8ce7eb8f231a3c27c3d9af0f14a88617013184
Linn <54114194+LinnJosefsson@users.noreply.github.com>
2020-12-11 16:27:02 +0100

    nu får det fan va nog.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ window.onload = function () {
     const dogOne = document.querySelector('.column .dogOne');
     const dogTwo = document.querySelector('.column .dogTwo');
     const dogThree = document.querySelector('.column .dogThree');
+   
      
 
 
@@ -15,69 +16,49 @@ window.onload = function () {
 
     //gör function av detta
     dogOne.addEventListener('click', function () {
-      
         const y = 220;
-        dogOne.style.marginTop = y + 'px';
-	
-        dogOne.style.position = 'fixed';
-        dogOne.style.width = '300' + 'px';
-        dogOne.style.transform = 'translateX(30%)';
-        dogOne.style.transition = 'width 2s';
         
+
+        dogOne.classList.add('dogOneStyle');
+    
+       
+    
         if (y === 220) {
             dogOne.onclick = function () {
-                dogOne.style.position = 'absolute';
-                dogOne.style.marginTop = 0;
-                dogOne.style.width = '120' + 'px';
+                
+                dogOne.classList.remove('dogOneStyle');
+                
+              
             }
         }
 
-        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-        if (viewportWidth > 640) {
-            dogOne.style.transform = 'translateX(110%)';
-        }
-        if (viewportWidth < 639) 
-            dogOne.style.transform = 'translateX(30%)';
-        
-
+    
     });
 
     dogTwo.addEventListener('click', function () {
         const y = 280;
         const x = 0;
-        dogTwo.style.marginTop = y + 'px';
-        dogTwo.style.position = 'fixed';
-        dogTwo.style.width = '240' + 'px';
-        dogTwo.style.transition = 'width 2s';
+        dogTwo.classList.add('dogTwoStyle');
+      
         
         if (y === 280) {
             dogTwo.onclick = function () {
-                dogTwo.style.position = 'absolute';
-                dogTwo.style.marginTop = x + 'px';
-                dogTwo.style.marginLeft = x + 'px';
-                dogTwo.style.width = '120' + 'px';
+                dogTwo.classList.remove('dogTwoStyle');
+             
             }
         }
 
     });
 
     dogThree.addEventListener('click', function () {
+        dogThree.classList.add('dogThreeStyle');
+        const y = 200;
+    
         
-        const y = 240;
-        const x = 0;
-        dogThree.style.marginTop = y + 'px';
-        dogThree.style.position = 'fixed';
-        dogThree.style.width = '200' + 'px';
-        dogThree.style.transform = 'translateX(-50%)';
-        dogThree.style.transition = 'width 2s';
-
-        
-        if (y === 240) {
+        if (y === 200) {
             dogThree.onclick = function () {
-                dogThree.style.position = 'absolute';
-                dogThree.style.marginTop = '0';
-                dogThree.style.width = '120' + 'px'; 
-                        
+                dogThree.classList.remove('dogThreeStyle');
+               
             }  
         }
 
@@ -112,8 +93,7 @@ window.onload = function () {
 			
     window.addEventListener("scroll", function (e) {
 		
-        // Scrollar upp? -> göm bilder
-        // Scrollar förbi första bilder - göm den, visa nästa
+       
 
         if (window.scrollY > 500 && window.scrollY < 1500) {
             document.querySelector('.vacation-0 img').style.display = 'block';
